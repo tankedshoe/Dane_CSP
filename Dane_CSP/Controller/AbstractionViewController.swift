e6d1fa8ce29062e85b6eea21a8dcabf9496ab857
tankedshoe <tankedshoe@gmail.com>
2017-11-27 08:45:56 -0700

    Added support for dots.

--- Dane_CSP/Controller/AbstractionViewController.swift
+++ Dane_CSP/Controller/AbstractionViewController.swift
@@ -94,6 +94,24 @@ class AbstractionViewController: UIPageViewController, UIPageViewControllerDataS
         return orderedAbstractionViews[nextIndex]
     }
     
+    //MARK:- Support for dots in the UIPageViewController
+    
+    public func presentationCount(for pageViewController: UIPageViewController) -> Int
+    {
+        return orderedAbstractionViews.count
+    }
+    
+    public func presentationIndex(for pageViewController: UIPageViewController) -> Int
+    {
+        guard let firstViewController = viewControllers?.first, let firstViewControllerIndex = orderedAbstractionViews.index(of: firstViewController)
+        else
+        {
+            return 0
+        }
+        
+        return firstViewControllerIndex
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
