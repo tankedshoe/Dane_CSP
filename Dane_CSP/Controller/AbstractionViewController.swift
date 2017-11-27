3e932ac2271e2ba794dd8225722423052ac7e1a6
tankedshoe <tankedshoe@gmail.com>
2017-11-27 08:09:20 -0700

    Added an array of subviews.

--- Dane_CSP/Controller/AbstractionViewController.swift
+++ Dane_CSP/Controller/AbstractionViewController.swift
@@ -8,14 +8,36 @@
 
 import UIKit
 
-class AbstractionViewController: UIViewController {
+class AbstractionViewController: UIPageViewController, UIPageViewControllerDataSource
+{
 
+    //MARK: Array of subviews
+    private (set) lazy var orderedAbstractionViews : [UIViewController] =
+    {
+        return [
+            self.newAbstractionViewController(abstractionLevel: "Block"),
+            self.newAbstractionViewController(abstractionLevel: "Java"),
+            self.newAbstractionViewController(abstractionLevel: "ByteCode"),
+            self.newAbstractionViewController(abstractionLevel: "Binary"),
+            self.newAbstractionViewController(abstractionLevel: "AndGate")
+        ]
+    }()
+    
+    //Helper method to retrieve the correct ViewController
+    private func newAbstractionViewController(abstractionLevel : String) -> UIViewController
+    {
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
 
+    //MARK:- Required protocol methods for UIPageViewControllerDataSource
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
