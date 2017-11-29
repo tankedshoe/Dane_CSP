8ba345a5efb111ebd9342dc1f0d319b72a55857f
tankedshoe <tankedshoe@gmail.com>
2017-11-29 08:23:24 -0700

    Added my Internet Master and Detail classes.

---
+++ Dane_CSP/Controller/InternetDetailViewController.swift
@@ -0,0 +1,38 @@
+//
+//  InternetDetailViewController.swift
+//  Dane_CSP
+//
+//  Created by Heaps, Dane on 11/29/17.
+//  Copyright © 2017 CTEC. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class InternetDetailViewController : UIViewController
+{
+    //MARK: GUI Controls
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Dane_CSP/Controller/InternetMasterViewController.swift
@@ -0,0 +1,68 @@
+//
+//  InternetMasterViewController.swift
+//  Dane_CSP
+//
+//  Created by Heaps, Dane on 11/29/17.
+//  Copyright © 2017 CTEC. All rights reserved.
+//
+
+import UIKit
+
+class InternetMasterViewController : UITableViewController
+{
+    private (set) lazy var internetTopics : [String] =
+    {
+        return [
+        "Definitions",
+        "CSP",
+        "CTEC",
+        "Canyons",
+        "Twitter",
+        "Start Guide",
+        ]
+    }()
+    
+    private var detailViewController : InternetDetailViewController?
+    
+    private func setup() -> Void
+    {
+        
+    }
+
+    override public func viewDidLoad() {
+        super.viewDidLoad()
+        setup()
+
+        self.clearSelectionOnViewWillAppear = false
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    //MARK: - Table view data source
+    
+    override public func numberOfSelections(in tableView: UITableView) -> Int
+    {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+    
+    override public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return internetTopics.count
+    }
+    
+    override public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        let cell = tableView.dequeueResuableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let currentText = internetTopics[indexPath.row]
+        cell.textLabel.text = currentText
+        
+        return cell
+    }
+   
+    
+
+}
