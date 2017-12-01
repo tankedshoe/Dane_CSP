cca295a457330e891dfdaa394b6c4ac35688791c
tankedshoe <tankedshoe@gmail.com>
2017-12-01 09:32:53 -0700

    Finished up InternetMasterViewController.

--- Dane_CSP/Controller/InternetMasterViewController.swift
+++ Dane_CSP/Controller/InternetMasterViewController.swift
@@ -22,11 +22,25 @@ class InternetMasterViewController : UITableViewController
         ]
     }()
     
+    private lazy var addresses : [String] = []
+    
     private var detailViewController : InternetDetailViewController?
     
     private func setup() -> Void
     {
+        addresses = [
+            "https://apstudent.collegeboard.org/apcourse/ap-computer-science-principles",
+            "http://www.canyonsdistrict.org/",
+            "https://ctec.canyonsdistrict.org/",
+            "https://developer.apple.com/library/content/documentation/Swift/Conceptual/Swift_Programming_Language/TheBasics.html#//apple_ref/doc/uid/TP40014097-CH5-ID309",
+            "https://twitter.com/?lang=en",
+        ]
         
+        if let splitView = splitViewController
+        {
+            let currentControllers = splitView.viewControllers
+            detailViewController = currentControllers[0] as? InternetDetailViewController
+        }
     }
 
     override public func viewDidLoad() {
@@ -61,6 +75,43 @@ class InternetMasterViewController : UITableViewController
         cell.textLabel.text = currentText
         
         return cell
+    }
+    
+    //MARK: Handle the internal transfer
+    override public func prepare(for segue: UIStoryboardSegue, sender: Any?)
+    {
+        if segue.identifier! == "showDetail"
+        {
+            if let indexPath = self.tableView.indexPathForSelectedRow
+            {
+                let urlString = addresses[indexPath.row]
+                let pageText : String
+                
+                if indexPath.row == 0
+                {
+                    pageText = """
+                    URL - the address of a World Wide Web page.
+                    
+                    TCP - a set of rules that governs the delivery of data over the Internet or other network that uses the Internet Protocol, and sets up a connection between the sending and receiving computers.
+                    
+                    IP - a set of rules governing the format of data sent over the Internet or other network.
+                    
+                    DNS - The Internet's system for converting alphabetic names into numeric IP addresses.
+                    """
+                }
+                else
+                {
+                    pageText = internetTopics[indexPath.row]
+                }
+                
+                let controller = segue.destination as! InternetDetailViewController
+                
+                controller.detailAddress = urlString
+                controller.detailText = pageText
+                controller.navigationItem.leftBarButtonItem = splitViewController?.displayModeButtonItem
+                controller.navigationItem.leftItemsSupplementBackButton = true
+            }
+        }
     }
    
     
