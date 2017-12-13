870ebbc1fe1e6a9144bae62ed36d01fd8ea0452f
tankedshoe <tankedshoe@gmail.com>
2017-12-13 08:36:15 -0700

    Set up the inital load thingy junk.

--- Dane_CSP/Controller/ProgrammingViewController.swift
+++ Dane_CSP/Controller/ProgrammingViewController.swift
@@ -7,11 +7,22 @@
 //
 
 import UIKit
+import SpriteKit
 
 class ProgrammingViewController: UIViewController {
 
-    override func viewDidLoad() {
+    override public func viewDidLoad() -> Void
+    {
         super.viewDidLoad()
+        let gameScene = StartScene(size: view.bounds.size)
+        let gameView = view as! SKView
+        
+        gameView.showsFPS = true
+        gameView.showsNodeCount = true
+        gameView.ignoresSiblingOrder = true
+        
+        gameScene.scaleMode = .resizeFill
+        gameView.presentScene(gameScene)
 
         // Do any additional setup after loading the view.
     }
