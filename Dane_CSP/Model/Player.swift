148acc1429a242c4bd1792725cb60e777fa2924c
tankedshoe <tankedshoe@gmail.com>
2017-12-13 09:55:04 -0700

    Added a lot of stuff. Trust me.

--- Dane_CSP/Model/Player.swift
+++ Dane_CSP/Model/Player.swift
@@ -35,6 +35,14 @@ public class Player: SKSpriteNode
         let texture = SKTexture(imageNamed: "x wing1")
         super.init(texture: texture, color: SKColor.clear, size: texture.size())
         
+        self.physicsBody = SKPhysicsBody(texture: self.texture!, size: self.size)
+        self.physicsBody?.isDynamic = true
+        self.physicsBody?.usesPreciseCollisionDetection = false
+        self.physicsBody?.categoryBitMask = CollisionCategories.Player
+        self.physicsBody?.contactTestBitMask = CollisionCategories.InvaderBullet | CollisionCategories.Invader
+        self.physicsBody?.collisionBitMask = CollisionCategories.EdgeBody
+        self.physicsBody?.allowsRotation = false
+        animate()
     }
     
     required public init?(coder aDecoder: NSCoder)
