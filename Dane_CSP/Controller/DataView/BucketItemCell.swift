c92ff314b0fa8d3c99799c1d7a82bca50f10325c
tankedshoe <tankedshoe@gmail.com>
2018-01-05 08:17:35 -0700

    Began setting up the Data Page.

---
+++ Dane_CSP/Controller/DataView/BucketItemCell.swift
@@ -0,0 +1,21 @@
+//
+//  BucketItemCell.swift
+//  Dane_CSP
+//
+//  Created by Heaps, Dane on 1/5/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+import UIKit
+
+class BucketItemCell: UITableViewCell {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ Dane_CSP/Model/BucketItemCell.swift
@@ -0,0 +1,9 @@
+//
+//  BucketItemCell.swift
+//  Dane_CSP
+//
+//  Created by Heaps, Dane on 1/5/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+import Foundation
