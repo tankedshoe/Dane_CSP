0982e2f0a3e4bb732a455fdf90ccf39bfff06777
tankedshoe <tankedshoe@gmail.com>
2018-01-09 08:11:31 -0700

    Built up the BucketItemCell.

---
+++ Dane_CSP/Controller/DataView/BucketItem.swift
@@ -0,0 +1,21 @@
+//
+//  BucketItem.swift
+//  Dane_CSP
+//
+//  Created by Heaps, Dane on 1/9/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+import UIKit
+
+class BucketItem: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
--- Dane_CSP/Controller/DataView/BucketItemCell.swift
+++ Dane_CSP/Controller/DataView/BucketItemCell.swift
@@ -8,14 +8,58 @@
 
 import UIKit
 
-class BucketItemCell: UITableViewCell {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+class BucketItemCell: UITableViewCell
+{
+    @IBOutlet weak var bucketItemText: UILabel!
+    @IBOutlet weak var bucketItemSymbol: UILabel!
+    @IBOutlet weak var bucketItemSignature: UILabel!
+    
+    var bucketItem : BucketItem!
+    {
+        didSet
+        {
+            updateCellView()
+        }
     }
-    */
-
+    
+    private func randomEmoji() -> String
+    {
+        let emojiStart = 0x1F601
+        let emojiEnd = 0x1F64F
+        let symbolStart = 0x1F680
+        let symbolEnd = 0x1F6C5
+        
+        let emojiRange = 79
+        let symbolRange = 70
+        
+        let ascii = emojiStart + Int(arc4random_uniform(UInt32(emojiRange)))
+        let emoji = UnicodeScalar(ascii)?.description
+        return emoji!
+    }
+    
+    private func updateCellView()
+    {
+        if (bucketItem != nil)
+        {
+            bucketItemSignature.text = bucketItem.itemAuthor
+            bucketItemText.text = bucketItem.itemContents
+        }
+        else
+        {
+            bucketItemSignature.text = "author goes here"
+            bucketItemText.text = "text goes here"
+        }
+        
+        bucketItemSymbol.text = randomEmojiSymbol()
+    }
+    
+    public override func awakeFromNib()
+    {
+        super.awakeFromNib()
+    }
+    
+    public override func setSelected(_ selected: Bool, animated: Bool)
+    {
+        super.setSelected(selected, animated: animated)
+    }
 }
