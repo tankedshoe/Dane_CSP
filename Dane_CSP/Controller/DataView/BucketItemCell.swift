515c2c03ea79c44f11a60a3e2b8eb398fee015a6
tankedshoe <tankedshoe@gmail.com>
2018-01-09 09:40:36 -0700

    Finished up the code for the programming page.

--- Dane_CSP/Controller/DataView/BucketItemCell.swift
+++ Dane_CSP/Controller/DataView/BucketItemCell.swift
@@ -25,12 +25,12 @@ class BucketItemCell: UITableViewCell
     private func randomEmoji() -> String
     {
         let emojiStart = 0x1F601
-        let emojiEnd = 0x1F64F
-        let symbolStart = 0x1F680
-        let symbolEnd = 0x1F6C5
+ //       let emojiEnd = 0x1F64F
+//        let symbolStart = 0x1F680
+//        let symbolEnd = 0x1F6C5
         
         let emojiRange = 79
-        let symbolRange = 70
+//        let symbolRange = 70
         
         let ascii = emojiStart + Int(arc4random_uniform(UInt32(emojiRange)))
         let emoji = UnicodeScalar(ascii)?.description
